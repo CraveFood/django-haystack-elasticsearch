a491e89b6bafb9e52430839dee805fa5768005d9
Bruno Marques <bruno@bmarques.net>
2016-12-27 18:40:37 -0200

    Refactored ES5.x backend

--- haystack_elasticsearch/elasticsearch5.py
+++ haystack_elasticsearch/elasticsearch5.py
@@ -72,24 +72,15 @@ class Elasticsearch5SearchBackend(ElasticsearchSearchBackend):
             else:
                 self.log.error("Failed to clear Elasticsearch index: %s", e, exc_info=True)
 
-    def build_search_kwargs(self, query_string, sort_by=None, start_offset=0, end_offset=None,
-                            fields='', highlight=False, facets=None,
-                            date_facets=None, query_facets=None,
-                            narrow_queries=None, spelling_query=None,
-                            within=None, dwithin=None, distance_point=None,
-                            models=None, limit_to_registered_models=None,
-                            result_class=None, **extra_kwargs):
-        index = haystack.connections[self.connection_alias].get_unified_index()
-        content_field = index.document_field
-
+    def _build_search_kwargs_default(self, content_field, query_string):
         if query_string == '*:*':
-            kwargs = {
+            return {
                 'query': {
                     "match_all": {}
                 },
             }
         else:
-            kwargs = {
+            return {
                 'query': {
                     'query_string': {
                         'default_field': content_field,
@@ -102,76 +93,29 @@ class Elasticsearch5SearchBackend(ElasticsearchSearchBackend):
                 },
             }
 
-        filters = []
-
+    def _build_search_kwargs_fields(self, fields):
         if fields:
             if isinstance(fields, (list, set)):
                 fields = " ".join(fields)
 
-            kwargs['stored_fields'] = fields
+        return 'stored_fields', fields
 
-        if sort_by is not None:
-            order_list = []
-            for field, direction in sort_by:
-                if field == 'distance' and distance_point:
-                    # Do the geo-enabled sort.
-                    lng, lat = distance_point['point'].get_coords()
-                    sort_kwargs = {
-                        "_geo_distance": {
-                            distance_point['field']: [lng, lat],
-                            "order": direction,
-                            "unit": "km"
-                        }
-                    }
-                else:
-                    if field == 'distance':
-                        warnings.warn(
-                            "In order to sort by distance, you must call the '.distance(...)' method.")
-
-                    # Regular sorting.
-                    sort_kwargs = {field: {'order': direction}}
-
-                order_list.append(sort_kwargs)
-
-            kwargs['sort'] = order_list
-
-        # From/size offsets don't seem to work right in Elasticsearch's DSL. :/
-        # if start_offset is not None:
-        #     kwargs['from'] = start_offset
-
-        # if end_offset is not None:
-        #     kwargs['size'] = end_offset - start_offset
-
-        if highlight:
-            # `highlight` can either be True or a dictionary containing custom parameters
-            # which will be passed to the backend and may override our default settings:
-
-            kwargs['highlight'] = {
-                'fields': {
-                    content_field: {},
-                }
+    def _build_search_kwargs_highlight(self, content_field, highlight):
+        result = {
+            'fields': {
+                content_field: {},
             }
+        }
+        if isinstance(highlight, dict):
+            result.update(highlight)
+        return result
 
-            if isinstance(highlight, dict):
-                kwargs['highlight'].update(highlight)
-
-        if self.include_spelling:
-            kwargs['suggest'] = {
-                'suggest': {
-                    'text': spelling_query or query_string,
-                    'term': {
-                        # Using content_field here will result in suggestions of stemmed words.
-                        'field': '_all',
-                    },
-                },
-            }
-
-        if narrow_queries is None:
-            narrow_queries = set()
+    def _build_search_kwargs_facets(self, facets, date_facets,query_facets):
+        result = {}
 
         if facets is not None:
-            kwargs.setdefault('aggs', {})
-
+            index = haystack.connections[self.connection_alias]\
+                .get_unified_index()
             for facet_fieldname, extra_options in facets.items():
                 facet_options = {
                     'meta': {
@@ -185,18 +129,18 @@ class Elasticsearch5SearchBackend(ElasticsearchSearchBackend):
                 }
                 if 'order' in extra_options:
                     facet_options['meta']['order'] = extra_options.pop('order')
-                # Special cases for options applied at the facet level (not the terms level).
+                # Special cases for options applied at the facet level
+                # (not the terms level).
                 if extra_options.pop('global_scope', False):
                     # Renamed "global_scope" since "global" is a python keyword.
                     facet_options['global'] = True
                 if 'facet_filter' in extra_options:
-                    facet_options['facet_filter'] = extra_options.pop('facet_filter')
+                    facet_options['facet_filter'] = extra_options.pop(
+                        'facet_filter')
                 facet_options['terms'].update(extra_options)
-                kwargs['aggs'][facet_fieldname] = facet_options
+                result[facet_fieldname] = facet_options
 
         if date_facets is not None:
-            kwargs.setdefault('aggs', {})
-
             for facet_fieldname, value in date_facets.items():
                 # Need to detect on gap_by & only add amount if it's more than one.
                 interval = value.get('gap_by').lower()
@@ -206,7 +150,7 @@ class Elasticsearch5SearchBackend(ElasticsearchSearchBackend):
                     # Just the first character is valid for use.
                     interval = "%s%s" % (value['gap_amount'], interval[:1])
 
-                kwargs['aggs'][facet_fieldname] = {
+                result[facet_fieldname] = {
                     'meta': {
                         '_type': 'date_histogram',
                     },
@@ -220,8 +164,10 @@ class Elasticsearch5SearchBackend(ElasticsearchSearchBackend):
                                 'field': facet_fieldname,
                                 'ranges': [
                                     {
-                                        'from': self._from_python(value.get('start_date')),
-                                        'to': self._from_python(value.get('end_date')),
+                                        'from': self._from_python(
+                                            value.get('start_date')),
+                                        'to': self._from_python(
+                                            value.get('end_date')),
                                     }
                                 ]
                             }
@@ -230,10 +176,8 @@ class Elasticsearch5SearchBackend(ElasticsearchSearchBackend):
                 }
 
         if query_facets is not None:
-            kwargs.setdefault('aggs', {})
-
             for facet_fieldname, value in query_facets:
-                kwargs['aggs'][facet_fieldname] = {
+                result[facet_fieldname] = {
                     'meta': {
                         '_type': 'query',
                     },
@@ -244,65 +188,23 @@ class Elasticsearch5SearchBackend(ElasticsearchSearchBackend):
                     },
                 }
 
-        for q in narrow_queries:
-            filters.append({
-                'query_string': {
-                    'query': q
-                }
-            })
+        return 'aggs', result
 
-        if within is not None:
-            filters.append(self._build_search_query_within(within))
-
-        if dwithin is not None:
-            filters.append(self._build_search_query_dwithin(dwithin))
-
-        # if we want to filter, change the query type to bool
-        if filters:
-            kwargs["query"] = {"bool": {"must": kwargs.pop("query")}}
-            if len(filters) == 1:
-                kwargs['query']['bool']["filter"] = filters[0]
-            else:
-                kwargs['query']['bool']["filter"] = {"bool": {"must": filters}}
-
-        if extra_kwargs:
-            kwargs.update(extra_kwargs)
-
-        return kwargs
-
-    def _build_search_query_dwithin(self, dwithin):
-        lng, lat = dwithin['point'].get_coords()
-        distance = "%(dist).6f%(unit)s" % {
-            'dist': dwithin['distance'].km,
-            'unit': "km"
-        }
+    def _build_search_filters_narrow_query(self, q):
         return {
-            "geo_distance": {
-                "distance": distance,
-                dwithin['field']: {
-                    "lat": lat,
-                    "lon": lng
-                }
+            'query_string': {
+                'query': q
             }
         }
 
-    def _build_search_query_within(self, within):
-        from haystack.utils.geo import generate_bounding_box
-        ((south, west), (north, east)) = generate_bounding_box(within['point_1'], within['point_2'])
-        return {
-            "geo_bounding_box": {
-                within['field']: {
-                    "top_left": {
-                        "lat": north,
-                        "lon": west
-                    },
-                    "bottom_right": {
-                        "lat": south,
-                        "lon": east
-                    }
-                }
-            },
-        }
+    def _build_search_kwargs_query(self, filters, query):
+        if filters:
+            result = {"bool": {"must": query}}
+            if len(filters) == 1:
+                result['bool']["filter"] = filters[0]
+            else:
+                result['bool']["filter"] = {"bool": {"must": filters}}
+        return result
 
     def more_like_this(self, model_instance, additional_query_string=None,
                        start_offset=0, end_offset=None, models=None,
